020e298e22eaec990d9e1183fbdffb409ddb31b1
husanswe <sulaymonovhusan2002@gmail.com>
2026-01-29 13:43:21 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('main', [PageController::class, 'main'])->name('main');
+Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('service', [PageController::class, 'service'])->name('service');
