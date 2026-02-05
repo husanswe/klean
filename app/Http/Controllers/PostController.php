9c0e77472f498675bd6f9c848334eb8f8b0b638a
husanswe <sulaymonovhusan2002@gmail.com>
2026-02-05 18:31:25 +0500

    learning laravel mvc models

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $post = Post::where('title', 'title')->first();
+        
+        dd($post);
+
         return view('posts.index');
     }
 
