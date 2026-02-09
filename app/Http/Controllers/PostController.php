31d5bd18b002db5feb35c9b97756b09dd413084c
husanswe <sulaymonovhusan2002@gmail.com>
2026-02-09 21:06:14 +0500

    fixed css style issue

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,7 +30,7 @@ class PostController extends Controller
     
     public function show(Post $post)
     {
-        return view('posts.show', [$post]);
+        return view('posts.show')->with('post', $post);
     }
 
     
