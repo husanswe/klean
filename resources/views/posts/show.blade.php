aac326b1af8923ea63e181fb96dc2e7b98380900
husanswe <sulaymonovhusan2002@gmail.com>
2026-02-10 12:59:37 +0500

    refactor post content display and update comments section title

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,87 +26,17 @@
 
                     <div class="mb-5">
                         <img class="img-fluid rounded w-100 mb-4" src="/img/carousel-1.jpg" alt="Image">
-                        <p>Sadipscing labore amet rebum est et justo gubergren. Et eirmod ipsum sit diam ut magna lorem.
-                            Nonumy vero labore lorem sanctus rebum et lorem magna kasd, stet amet magna accusam
-                            consetetur
-                            eirmod. Kasd accusam sit ipsum sadipscing et at at sanctus et. Ipsum sit gubergren dolores
-                            et,
-                            consetetur justo invidunt at et aliquyam ut et vero clita. Diam sea sea no sed dolores diam
-                            nonumy, gubergren sit stet no diam kasd vero.</p>
-                        <p>Voluptua est takimata stet invidunt sed rebum nonumy stet, clita aliquyam dolores vero stet
-                            consetetur elitr takimata rebum sanctus. Sit sed accusam stet sit nonumy kasd diam dolores,
-                            sanctus lorem kasd duo dolor dolor vero sit et. Labore ipsum duo sanctus amet eos et.
-                            Consetetur
-                            no sed et aliquyam ipsum justo et, clita lorem sit vero amet amet est dolor elitr, stet et
-                            no
-                            diam sit. Dolor erat justo dolore sit invidunt.</p>
-                        <h2 class="mb-4">Est dolor lorem et ea</h2>
-                        <img class="img-fluid rounded w-50 float-left mr-4 mb-3" src="/img/blog-1.jpg" alt="Image">
-                        <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor invidunt at
-                            est
-                            sanctus sanctus. Clita dolores sit kasd diam takimata justo diam lorem sed. Magna amet sed
-                            rebum
-                            eos. Clita no magna no dolor erat diam tempor rebum consetetur, sanctus labore sed nonumy
-                            diam
-                            lorem amet eirmod. No at tempor sea diam kasd, takimata ea nonumy elitr sadipscing gubergren
-                            erat. Gubergren at lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
-                            sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam dolores
-                            takimata
-                            dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna sea at sed et eos. Accusam
-                            eirmod
-                            kasd lorem clita sanctus ut consetetur et. Et duo tempor sea kasd clita ipsum et. Takimata
-                            kasd
-                            diam justo est eos erat aliquyam et ut. Ea sed sadipscing no justo et eos labore, gubergren
-                            ipsum magna dolor lorem dolore, elitr aliquyam takimata sea kasd dolores diam, amet et est
-                            accusam labore eirmod vero et voluptua. Amet labore clita duo et no. Rebum voluptua magna
-                            eos
-                            magna, justo gubergren labore sit voluptua eos.</p>
-                        <h3 class="mb-4">Est dolor lorem et ea</h3>
-                        <img class="img-fluid rounded w-50 float-right ml-4 mb-3" src="/img/blog-2.jpg" alt="Image">
-                        <p>Diam dolor est labore duo invidunt ipsum clita et, sed et lorem voluptua tempor invidunt at
-                            est
-                            sanctus sanctus. Clita dolores sit kasd diam takimata justo diam lorem sed. Magna amet sed
-                            rebum
-                            eos. Clita no magna no dolor erat diam tempor rebum consetetur, sanctus labore sed nonumy
-                            diam
-                            lorem amet eirmod. No at tempor sea diam kasd, takimata ea nonumy elitr sadipscing gubergren
-                            erat. Gubergren at lorem invidunt sadipscing rebum sit amet ut ut, voluptua diam dolores at
-                            sadipscing stet. Clita dolor amet dolor ipsum vero ea ea eos. Invidunt sed diam dolores
-                            takimata
-                            dolor dolore dolore sit. Sit ipsum erat amet lorem et, magna sea at sed et eos. Accusam
-                            eirmod
-                            kasd lorem clita sanctus ut consetetur et. Et duo tempor sea kasd clita ipsum et. Takimata
-                            kasd
-                            diam justo est eos erat aliquyam et ut. Ea sed sadipscing no justo et eos labore, gubergren
-                            ipsum magna dolor lorem dolore, elitr aliquyam takimata sea kasd dolores diam, amet et est
-                            accusam labore eirmod vero et voluptua. Amet labore clita duo et no.</p>
+                        <p>{{ $post->content }}</p>
                     </div>
 
                     <div class="mb-5">
-                        <h3 class="mb-4 section-title">3 Comments</h3>
+                        <h3 class="mb-4 section-title">Comments</h3>
                         <div class="media mb-4">
                             <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1" style="width: 45px;">
                             <div class="media-body">
                                 <h6>John Doe <small><i>01 Jan 2045 at 12:00pm</i></small></h6>
                                 <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita, at tempor amet ipsum diam tempor sit.</p>
                                 <button class="btn btn-sm btn-light">Reply</button>
-                            </div>
-                        </div>
-                        <div class="media mb-4">
-                            <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1" style="width: 45px;">
-                            <div class="media-body">
-                                <h6>John Doe <small><i>01 Jan 2045 at 12:00pm</i></small></h6>
-                                <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita, at tempor amet ipsum diam tempor sit.</p>
-                                <button class="btn btn-sm btn-light">Reply</button>
-                                <div class="media mt-4">
-                                    <img src="/img/user.jpg" alt="Image" class="img-fluid rounded-circle mr-3 mt-1"
-                                        style="width: 45px;">
-                                    <div class="media-body">
-                                        <h6>John Doe <small><i>01 Jan 2045 at 12:00pm</i></small></h6>
-                                        <p>Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum clita, at tempor amet ipsum diam tempor sit.</p>
-                                        <button class="btn btn-sm btn-light">Reply</button>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
