a3d1de5e8c83f2f42b2548f6cf46ac35f8bb54f9
husanswe <sulaymonovhusan2002@gmail.com>
2026-02-15 17:03:34 +0500

    fix: add max file size validation for photo upload in StorePostRequest

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -34,7 +34,7 @@ class StorePostRequest extends FormRequest
             'title' => 'required|max:255',
             'short_content' => 'required',
             'content' => 'required',
-            'photo' => 'nullable|image'
+            'photo' => 'nullable|image|max:2048'
         ];
     }
 }
