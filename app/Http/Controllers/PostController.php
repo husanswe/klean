ff439c3faf1a9e030c624edeb155cb5dd3639c58
husanswe <sulaymonovhusan2002@gmail.com>
2026-02-17 12:34:39 +0500

    fix: update post retrieval to order by latest before pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
 {
     public function index()
     {   
-        $posts = Post::paginate(3);
+        $posts = Post::latest()->paginate(3);
 
         return view('posts.index')->with('posts', $posts);
     }
