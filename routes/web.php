925cd84426d944cee4c110e2588b0075808b8f5e
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-04 17:14:57 +0500

    created UserController.php file

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,21 @@
 <?php
 
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\PostController; 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\PageController;
+    use App\Http\Controllers\PostController;
+    use App\Http\Controllers\CommentController;
+    use App\Http\Controllers\UserController;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('main', [PageController::class, 'main'])->name('main');
-Route::get('about', [PageController::class, 'about'])->name('about');
-Route::get('service', [PageController::class, 'service'])->name('service');
-Route::get('project', [PageController::class, 'project'])->name('project');
-Route::get('contact', [PageController::class, 'contact'])->name('contact');
+    Route::get('main', [PageController::class, 'main'])->name('main');
+    Route::get('about', [PageController::class, 'about'])->name('about');
+    Route::get('service', [PageController::class, 'service'])->name('service');
+    Route::get('project', [PageController::class, 'project'])->name('project');
+    Route::get('contact', [PageController::class, 'contact'])->name('contact');
 
-Route::resource('posts', PostController::class);
-
-
-/* Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('posts/create', [PostController::class, 'store'])->name('posts.store');
-Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('posts/{post}/edit', [PostController::class, 'update'])->name('posts.update');
-Route::delete('posts/{post}/delete', [PostController::class, 'delete'])->name('posts.delete'); */
-
+    Route::resources([
+        'posts' => PostController::class,
+        'comments' => CommentController::class,
+        'users' => UserController::class,     
+    ]);
 
 ?>
