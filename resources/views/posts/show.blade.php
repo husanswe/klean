b07483fd24ff191279cd6ade02f1132544d6470d
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-04 17:02:17 +0500

    added method attribute to form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -60,24 +60,25 @@
 
                     <div class="bg-light rounded p-5">
                         <h3 class="mb-4 section-title">Leave a comment</h3>
-                        <form>
-                            {{-- <div class="form-row">
-                                <div class="form-group col-sm-6">
-                                    <label for="name">Name *</label>
-                                    <input type="text" class="form-control" id="name">
-                                </div>
-                                <div class="form-group col-sm-6">
-                                    <label for="email">Email *</label>
-                                    <input type="email" class="form-control" id="email">
-                                </div>
+                        {{-- <div class="form-row">
+                            <div class="form-group col-sm-6">
+                                <label for="name">Name *</label>
+                                <input type="text" class="form-control" id="name">
                             </div>
+                            <div class="form-group col-sm-6">
+                                <label for="email">Email *</label>
+                                <input type="email" class="form-control" id="email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="website">Website</label>
+                            <input type="url" class="form-control" id="website">
+                        </div> --}}
+                        
+                        <form action="" method="post">
+                            @csrf
                             <div class="form-group">
-                                <label for="website">Website</label>
-                                <input type="url" class="form-control" id="website">
-                            </div> --}}
-
-                            <div class="form-group">
-                                <label for="message">Message *</label>
+                                <label for="message">Message</label>
                                 <textarea id="message" cols="30" rows="5" class="form-control"></textarea>
                             </div>
                             <div class="form-group mb-0">
