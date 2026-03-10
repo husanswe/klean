5711e63da8fa1bfce38e114aa81070c91fb2721d
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-10 08:12:08 +0500

    fixed my mistake in create tags table.php

--- database/migrations/2026_03_09_130437_create_post_tag_table.php
+++ database/migrations/2026_03_09_130437_create_post_tag_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->foreignId('post_id')->constrained();
             $table->foreignId('tag_id')->constrained();
             $table->timestamps();
