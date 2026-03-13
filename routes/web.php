4ad11deb9b781136a543a1531af76b362d9aa328
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-13 16:35:25 +0500

    building login and sign up pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-    use App\Http\Controllers\PageController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PageController;
     use App\Http\Controllers\PostController;
     use App\Http\Controllers\CommentController;
     use App\Http\Controllers\UserController;
@@ -11,6 +12,9 @@
     Route::get('service', [PageController::class, 'service'])->name('service');
     Route::get('project', [PageController::class, 'project'])->name('project');
     Route::get('contact', [PageController::class, 'contact'])->name('contact');
+
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+
 
     Route::resources([
         'posts' => PostController::class,
