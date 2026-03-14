34b29eb53e61254e254849fc6fb9f3e09fb4efdd
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-14 16:06:24 +0500

    hashed password during user registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\RedirectResponse;
 use App\Models\User;
 
@@ -45,6 +46,8 @@ class AuthController extends Controller
             'password' => 'required|min:8',
             'password_confirmation' => 'required|same:password'
         ]);
+
+        $validated['password'] = Hash::make($validated['password']);
 
         $user = User::create($validated);
 
