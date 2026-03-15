7b107d7f17176b6500b36eab87e5776d26642f07
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-15 12:39:11 +0500

    fix: improved comment prompt text and display post author's name

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -83,7 +83,7 @@
                             </form>
                         @else
                             <div>
-                                Login to leave comment
+                                Login to leave a comment
                                 <a href="{{ route('login') }}" class="btn btn-primary ml-3">Login</a>
                             </div>
                         @endauth
@@ -93,7 +93,7 @@
                 <div class="col-lg-4 mt-5 mt-lg-0">
                     <div class="d-flex flex-column text-center bg-secondary rounded mb-5 py-5 px-4">
                         <img src="/img/user.jpg" class="img-fluid rounded-circle mx-auto mb-3" style="width: 100px;">
-                        <h3 class="text-white mb-3">John Doe</h3>
+                        <h3 class="text-white mb-3">{{ $post->user->name }}</h3>
                         <p class="text-white m-0">Conset elitr erat vero dolor ipsum et diam, eos dolor lorem ipsum,
                             ipsum
                             ipsum sit no ut est. Guber ea ipsum erat kasd amet est elitr ea sit.</p>
