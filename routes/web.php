e9730953321ad1620e3d8b412a25d2376ef289ae
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-27 10:22:09 +0500

    left space for in 2 lines

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PageController;
+    use App\Http\Controllers\AuthController;
+    use App\Http\Controllers\PageController;
     use App\Http\Controllers\PostController;
     use App\Http\Controllers\CommentController;
     use App\Http\Controllers\UserController;
