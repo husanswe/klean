fa477c830a516eecf291605bf372cb596e65cad1
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-29 18:36:28 +0500

    added authorization to edit and delete buttons under comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,9 +6,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Comment;
 use App\Models\Post;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class CommentController extends Controller
 {
+    use AuthorizesRequests;
+    
     public function store(Request $request) {
         $comment = Comment::create([
             'body' => $request->body,
@@ -18,4 +21,17 @@ class CommentController extends Controller
 
         return redirect()->back();
     }
+
+    public function update(Request $request, Comment $comment) {
+        $this->authorize('update', $comment);
+        $comment->update(['body' => $request->body]);
+        return redirect()->back();
+    }
+
+    public function destroy(Request $request, Comment $comment) {
+        $this->authorize('delete', $comment);
+        $comment->delete();
+        return redirect()->back();
+    }
+
 }
