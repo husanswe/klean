a1ee7c33a3d0ae13e2d3667cde2d7a13a2aa4a0d
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-29 19:07:46 +0500

    fixed remember me checkbox

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
             'password' => ['required'],
         ]);
  
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
  
             return redirect()->intended('main');
