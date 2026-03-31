351cd31e8b191b554bd1749dcf4c70fa889fd3b7
husanswe <sulaymonovhusan2002@gmail.com>
2026-03-31 20:27:02 +0500

    defined event in PostCreated.php event file

--- app/Events/PostCreated.php
+++ app/Events/PostCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Post;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,12 +15,14 @@ class PostCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $post;
+
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(Post $post)
     {
-        //
+        $this->post = $post;
     }
 
     /**
