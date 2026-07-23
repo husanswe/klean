280c79d78817add87a609a3663f30aa6d2a1357b
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-23 23:30:16 +0500

    removed unnecessary comments from PostCreated notification class

--- app/Notifications/PostCreated.php
+++ app/Notifications/PostCreated.php
@@ -11,9 +11,6 @@ class PostCreated extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
     public function __construct()
     {
         //
@@ -29,9 +26,6 @@ class PostCreated extends Notification
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
