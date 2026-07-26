d7d204c6c0d0b28cce504fbde3b6700eef5f797b
husanswe <sulaymonovhusan2002@gmail.com>
2026-07-26 08:27:12 +0500

    added Postman resources and globals configuration, and implement scheduled logging for user, post, and comment counts

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function() {
+    $userCount = User::count();
+    $postCount = Post::count();
+    $commentCount = Comment::count();
+    
+    Log::info("Counting how many new users, posts and comments created");
+})->everyMinute();
