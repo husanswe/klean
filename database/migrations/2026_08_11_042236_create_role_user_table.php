48a19de4cb111463c1136eea7f9f15ab8a21cdc0
husanswe <sulaymonovhusan2002@gmail.com>
2026-08-11 09:41:52 +0500

    created role_user pivot table

--- database/migrations/2026_08_11_042236_create_role_user_table.php
+++ database/migrations/2026_08_11_042236_create_role_user_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_tag', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('role_user');
     }
 };
